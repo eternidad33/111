fff75d92449b7c9880b644f366bc9be26ee4d821
nonoupload <oppo95149@gmail.com>
2019-12-10 15:49:03 +0800

    俄罗斯

--- CCF CSP/Second/俄罗斯方块.py
+++ CCF CSP/Second/俄罗斯方块.py
@@ -9,6 +9,24 @@ x=int(input())
 #将形状转换为4行10列
 for i in range(4):
     shape[i]=[0]*(x-1)+shape[i]+[0]*(6-(x-1))
+find=False
+for i in range(3):
+    tmap.append([1]*10)
+for i in range(15):
+    for j in range(4):
+        newline=[a+b for a,b in zip(tmap[i+j],shape[j])]
+        if(2 in newline):
+            right=i-1
+            find=True
+            break
+    if(find):
+        break
+    if i==14:
+        right=14
+for i in range(4):
+    tmap[right+i]=[a+b for a,b in zip(tmap[right+i],shape[i])]
+for i in range(15):
+    print(' '.join(map(str,tmap[i])))
 
 
 '''
