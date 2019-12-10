e729af423003b943bc88ed07765fdba50506e1bf
nonoupload <oppo95149@gmail.com>
2019-12-10 18:09:23 +0800

    100

--- CCF CSP/Second/买菜.py
+++ CCF CSP/Second/买菜.py
@@ -4,17 +4,14 @@ for i in range(n):
     H.append(list(map(int,input().split())))
 for i in range(n):
     W.append(list(map(int,input().split())))
-tsum=0
-for h in H:
-    for w in W:
-        if h[0]>=w[0]:
-            if w[1]>=h[0] and w[1]<=h[1]:
-                tsum+=(w[1]-h[0])
-            if w[1]>=h[1]:
-                tsum+=(h[1]-h[0])
-        else :
-            if h[1]>=w[0] and h[1]<=w[1]:
-                tsum+=(h[1]-w[0])
-            if h[1]>=w[1]:
-                tsum+=(w[1]-w[0])
-print(tsum)
+tlen=max(H[-1][-1],W[-1][-1])
+timeLine=[0]*tlen
+for s,e in H:
+    for i in range(s,e):
+        timeLine[i]+=1
+for s,e in W:
+    for i in range(s,e):
+        timeLine[i]+=1
+print(timeLine.count(2))
+    
+
