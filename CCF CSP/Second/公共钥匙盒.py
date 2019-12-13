eb5918816b9ede8a208f9c7e8e2f3599d6e09058
nonoupload <oppo95149@gmail.com>
2019-12-13 16:20:57 +0800

    超时90

--- CCF CSP/Second/公共钥匙盒.py
+++ CCF CSP/Second/公共钥匙盒.py
@@ -3,8 +3,7 @@ key=list(range(1,n+1))
 info=[]
 for i in range(k):
     info.append(list(map(int,input().split())))
-for i in range(k):
-    info[i][2] = info[i][1] + info[i][2]
+
 start_time=[i[1] for i in info]
 end_time=[i[2] for i in info]
 
@@ -27,6 +26,7 @@ for t in range(1,max(end_time)+1):
     if t in start_time:
         lend_key=return_lend_key(info,start_time)
         for r in lend_key:
-            if key[i]==r:
-                key[i]=0
+            for i in range(n):
+                if key[i]==r:
+                    key[i]=0
 print(' '.join(map(str,key)))
